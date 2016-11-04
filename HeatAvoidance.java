6f6ef78d06a8c18db4924fdfcd467a75ed1ff561
Crystal Ren <crystalren97@gmail.com>
2016-11-04 12:49:55 -0400

    Update HeatAvoidance.java

--- HeatAvoidance.java
+++ HeatAvoidance.java
@@ -1,3 +1,4 @@
+
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.*;
 import org.opencv.features2d.*;
@@ -6,57 +7,39 @@ import org.opencv.highgui.VideoCapture;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-//code from http://dummyscodes.blogspot.com/2015/12/using-siftsurf-for-object-recognition.html
+import java.util.Scanner;
 public class HeatAvoidance {
 
 	public static void main(String args[]){
 		
-		//BufferedWriter bw = null;
-		//try {
-			/**
-			File file = new File("C:/Users/Crystal Ren/Desktop/data.txt");
-			if (!file.exists()) {
-		   	     file.createNewFile();
-		   	  }
-			 FileWriter fw = new FileWriter(file);
-		   	  bw = new BufferedWriter(fw);
-		   	  
-	*/
-		
-		//28 min video
+
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		
-		System.setProperty("java.library.path", "C:/Users/Crystal Ren/eclipse/jee-neon/eclips/opencv/build/java/x64/opencv_ffmpeg2413_64");
-		System.loadLibrary("opencv_ffmpeg2413_64"); 
+		Scanner sc = new Scanner(System.in);
+		String testerVid = sc.next();
 
-	//String testerVid ="C:/Users/Crystal Ren/Desktop/ButterflyVideos/Visible/non_AVI/ButterflyVid edited_trimmed.mp4";
-
-		  String testerVid ="C:/Users/Crystal Ren/Desktop/ButterflyVideos/Visible/non_AVI/heatavoidancecalibrate.avi";
-		   //Rec-butterfly_9_clod_15C_30p_cropped_1_2000
 		     VideoCapture capture = new VideoCapture(testerVid);
-			//capture.open(testerVid);
-				//System.out.println("total frames:"+capture.get(7));
+
 		     Mat video_image=new Mat();  
-
-		     int frame =0;
-	
-
+		  
+		     int frame = 0;
+		     int count=0;
+		     Point[] track=new Point[2];
+		     Point[] exVals = new Point[4];
+ 			 Point[] majorAxis = new Point[2];
+ 			 Point oldMidpt = new Point();
+ 			 double oldAngle=0.0;
+ 			 double newAngle =0.0;
 		     //to do: find two lines of same intensity along the triangular shadow area (so that the light source can be traced)
 		     
-		     
+
 	       if( capture.isOpened())  //capture is the video file
 		     { 
 	    	
@@ -64,94 +47,157 @@ public class HeatAvoidance {
 		      while( true )  
 		      {  
 		    	  frame++;
-		        capture.read(video_image);
-		 
+		    		  capture.read(video_image);
 		        
-		        //TimeUnit.SECONDS.sleep(1);
 		        if (video_image.empty()){
-			           System.out.println(" --(!) No captured frame -- Break!");  
+			           System.out.println(" --(!) No captured frame--");  
 			           break; 
 		        } 
 		       
 	        capture.read(video_image); //reads captured frame into the Mat image
-    		Functions.displayImage(Functions.Mat2BufferedImage(video_image));
+	        
 
-	        int centerX =video_image.cols()/2-30;
-			int centerY =video_image.rows()/2-10;
-			int radius = 330;
-
-			Point center= new Point(centerX,centerY);
 			
 	        		//Mat video_image= video_image.clone();
 	    			Imgproc.cvtColor(video_image, video_image, Imgproc.COLOR_BGR2GRAY);
-	    			Imgproc.threshold(video_image, video_image, 105, 255, Imgproc.THRESH_BINARY); //75 might work
-//was 145
-	    			//Imgproc.threshold(video_image, video_image, 120, 255, Imgproc.THRESH_BINARY_INV); //thresh was 55 changed to 75
-	        		Functions.displayImage(Functions.Mat2BufferedImage(video_image));
-
-
-	    			//make a circle of radius 507px and make everything outside of it white
-	    			//Mat mask = Mat.zeros( video_image.rows()+2,video_image.cols()+2, CvType.CV_8UC1 );
-		 			Mat mask = Mat.zeros(video_image.rows()+2,video_image.cols()+2, CvType.CV_8UC1 );
-	    			Core.ellipse(mask, center, new Size(325,270), 10, 0, 360, new Scalar(255,255,255), 1);
-	    			Core.ellipse(video_image, center, new Size(325,270), 10, 0, 360, new Scalar(255,255,255),1);
-		    		Functions.displayImage(Functions.Mat2BufferedImage(video_image));
-
-	    			Point tster = new Point(centerX+300,centerY+200);
-	    			Point tster2 = new Point(centerX+300,centerY-50);
-	    			Point tster3 = new Point(centerX-30,centerY+240);
-	    			//Core.circle(video_image, tster, 5, new Scalar(255,255,255));
-	        		
-
-					//Core.circle(mask, center, radius, new Scalar(255,255,255), 3);
-
-	    			Imgproc.floodFill(video_image, mask, tster3, new Scalar(255,255,255));
-	    			Imgproc.floodFill(video_image, mask, new Point(0,video_image.rows()/2), new Scalar(255,255,255));
-
-	    			Imgproc.floodFill(video_image, mask, new Point(video_image.cols()-1,0), new Scalar(255,255,255));
-	    			Imgproc.floodFill(video_image, mask, tster2, new Scalar(255,255,255));
-	    			Imgproc.floodFill(video_image, mask, new Point(video_image.cols()-1,video_image.rows()-1), new Scalar(255,255,255));
-	    			Imgproc.floodFill(video_image, mask, tster, new Scalar(255,255,255));
-					//Core.circle(video_image, center, radius, new Scalar(255,255,255), 3);
-
-	    			//Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(2,2)), new Point(-1,-1),3);
-	    			Imgproc.dilate(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_DILATE, new Size(2,2)));
-	    			Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(2,2)));
-
-	    			//Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(3,3)));
-
 	    			
 
-	        		//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
+	
+	    				Imgproc.threshold(video_image, video_image, 95, 255, Imgproc.THRESH_BINARY);
+	    				//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
+	    				
+	    				Imgproc.dilate(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_DILATE, new Size(3,3)));
+	    				Imgproc.dilate(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_DILATE, new Size(3,3)));
+
+	    				Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(3,3)));
+	    				Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(3,3)));
+	    				Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(3,3)));
+	    				Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(3,3)));
+	    				Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(3,3)));
+	    				
+	    				Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(3,3)));
+	    				//Imgproc.dilate(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_DILATE, new Size(3,3)));
+		    			//Imgproc.dilate(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_DILATE, new Size(3,3)));
+	    				//Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(3,3)));
+	    				//Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(3,3)));
+	    				//Core.putText(video_image, "Frame"+frame,new Point(55, 150) , 3,1.0,new Scalar(255,255,255),3); //for test
+	    				//}
+	    				//else if (frame>=504){
+
+		    			
+			    		//Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(4,4)));
+
+	    					
+	    				//}
+		    			Imgproc.adaptiveThreshold(video_image, video_image, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 15,2); 
+		    			Imgproc.dilate(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_DILATE, new Size(3,3)));
+		    			//Imgproc.erode(video_image, video_image, Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(4,4)));
+
+		    			//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
 	    			
 	    			Core.bitwise_not(video_image, video_image);
-	        		//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
+	    			
+			      //below horizontal and vertical to remove unwanted contours in image
+	    			//horizontal
+	    			if(frame<=105 || frame>=407){
+	    			for(int i=0;i<video_image.cols();i++){
+		    			 for(int j=10;j<100;j++){
+		    				 video_image.put(j, i, 0);
+		    				 }
+		    			}
+	    			}
+	    			else if(frame>=380){
+	    				for(int i=0;i<video_image.cols();i++){
+			    			 for(int j=10;j<50;j++){
+			    				 video_image.put(j, i, 0);
+			    				 }
+			    			}	
+	    			}
+	    			
+	    			if(frame==0){
+		    			for(int i=0;i<video_image.cols();i++){
+			    			 for(int j=10;j<200;j++){
+			    				 video_image.put(j, i, 0);
+			    				 }
+			    			}
+		    			}
+	    			
+	    			//horizontal
+	    			//if(frame<=199){
+	    			for(int i=0;i<video_image.cols();i++){
+		    			 for(int j=400;j<600;j++){
+		    				 video_image.put(j, i, 0);
+		    				 }
+		    			}
+	    			//}
+	    			//vertical
+		    			for(int i=0;i<300;i++){
+			    			 for(int j=0;j<video_image.rows();j++){
+			    				 video_image.put(j, i, 0);
+			    				 }
+			    			}
 
-	    			//Core.circle(video_image, tster2, 5, new Scalar(0,0,0));
-	        		//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
+		    		//vertical
+		    			if(frame>=1){
+		    				for(int i=650;i<800;i++){
+				    			 for(int j=0;j<video_image.rows();j++){
+				    				 video_image.put(j, i, 0);
+				    				 }
+				    			}
+		      }
+		    			if(frame>=360){
+		    				//vertical
+		    				for(int i=600;i<800;i++){
+				    			 for(int j=0;j<video_image.rows();j++){
+				    				 video_image.put(j, i, 0);
+				    				 }
+				    			}
+		    				
+		    				
+		    			}
+		    			
+	
 
+		    			
 	    			ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-	    			Imgproc.findContours(video_image, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-	    			contours.trimToSize();
-	    			Imgproc.drawContours(video_image, contours, -1, new Scalar(255,255,255),2);
-	    		
-		    		//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
+	    			Imgproc.findContours(video_image, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+	    			
 
+	    			
+	    			//Imgproc.drawContours(video_image, contours, -1, new Scalar(255,255,255), -1); //drawing filled contours
+
+	    			//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
+	    			
+
+	    			
+	    			ArrayList<MatOfPoint> contours2 = new ArrayList<MatOfPoint>();
+	    			
+
+	    			Imgproc.findContours(video_image, contours2, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+
+	    			contours2.trimToSize();
+	    			Imgproc.drawContours(video_image, contours2, -1, new Scalar(255,255,255),-1);
+	    			
+	    			//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
+
+	    			
 	    			 //int maxIdx=Functions.getLargestContour(contours);
+	    			
+	    			if(contours2.size()!=0){
+	    				
+	    				count++;
 	    			double maxArea = -1;
 	    			int maxAreaIdx = -1;
 	    			MatOfPoint temp_contour; //contours.get(0); //the largest is at the index 0 for starting point
-	    			MatOfPoint largest_contour = contours.get(0);
-	    			   
-	    			for( int i = 0; i < contours.size(); i++ ){
-	    				temp_contour = contours.get(i);
-    			        Rect rect = Imgproc.boundingRect(temp_contour);
-    			        //debugging
-        	     		//Core.rectangle(video_image, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar(255,0,0), 3); //for testing
-
-
-	    	     		//if ((Math.pow((rect.x-centerX+radius),2)+Math.pow((rect.y-centerY),2))<radsq){
-	    	     			if(rect.x>50 && rect.x+rect.width<725&&rect.y+rect.height<655){
+	    			MatOfPoint largest_contour = contours2.get(0);
+	    			
+   
+	    			
+	    			for( int i = 0; i < contours2.size(); i++ ){
+	    				temp_contour = contours2.get(i);
+   			       Rect rect = Imgproc.boundingRect(temp_contour);
+   			       if(rect.x<600 && rect.x>250 && rect.y>10 && rect.y<400){
+   			    	   
 	    	     			 double contourArea = Imgproc.contourArea(temp_contour);
 	    	     			if (contourArea > maxArea) {
 	    	     				maxArea=contourArea;
@@ -161,53 +207,113 @@ public class HeatAvoidance {
 	    	     		}
 	    	     		
 	    			}
-    	     		//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
 
-    	     		Rect maxRect = Imgproc.boundingRect(largest_contour); //for testing
-    	     		Core.rectangle(video_image, new Point(maxRect.x,maxRect.y), new Point(maxRect.x+maxRect.width,maxRect.y+maxRect.height), new Scalar(255,0,0), 3); //for testing
+	    			Imgproc.drawContours(video_image, contours2, -1, new Scalar(255,255,255), -1); //drawing filled contours
+
+	        		
+	    			//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
+
+	        		
+	    			//RADIUS FOR CHOOSING HERE
+	    			exVals = Functions.connectTailHead(largest_contour, contours2, 100, 1500, video_image);
+	    			majorAxis=Functions.getOrientation(exVals);
+	    			
+
+	    			Core.circle(video_image, exVals[0], 5, new Scalar(255,255,255),3);
+	    			Core.circle(video_image, exVals[1], 5, new Scalar(255,255,255),3);
+	    			Core.circle(video_image, exVals[2], 5, new Scalar(255,255,255),3);
+	    			Core.circle(video_image, exVals[3], 5, new Scalar(255,255,255),3);
+	    			//Core.circle(video_image, exVals[3], 60, new Scalar(255,255,255),3);
+
+	    			
+	    			if(count==1){
+	    				track=majorAxis;
+	    				//Core.line(video_image, track[0], track[1], new Scalar(255,255,255));
+	    				Core.line(video_image, exVals[1], exVals[2], new Scalar(0,0,0)); //testing&calibration
+	    				//System.out.println("TEST");//testing
+	    				//for this particular video clip either 0,1 or 2,1
+	    				//Functions.laserAvoidanceCalibration(exVals, video_image);
+	    				track[0]=exVals[1];
+	    				track[1]=exVals[2];
+	    				oldMidpt=Functions.getMidpoint(track);
+	    				//System.out.println(Functions.yDist(oldMidpt));
+
+	    				//oldAngle=Functions.getAngleTwoLines(track);
+	    				//so in this case have to force fit the orientation to the first two values of exVals
+	    				//Core.circle(video_image, majorAxis[0], 5, new Scalar(255,255,255),3);
+		    			//Core.circle(video_image, majorAxis[1], 5, new Scalar(255,255,255),3);
+		    			//Functions.displayImage(Functions.Mat2BufferedImage(video_image)); //testing
+		    		
+	    			}
+	    			
+	    			/*
+	    			else if(frame==280){
+	    				track[0]=exVals[1];
+	    				track[1]=exVals[2];
+	    				track = Functions.trackOrientation(track, exVals);
+	    				Point nextMidpt = Functions.getMidpoint(track);
+	    				//System.out.println(Functions.yDist(nextMidpt));
+	    				System.out.println(Functions.getAngleTwoLines(track)); //prints just angle
+	    				//"frame: "+frame+ " " + 
+	    				oldAngle = newAngle;
+	    				Core.line(video_image, track[0], track[1], new Scalar(0,0,0));
+		    			//Core.circle(video_image, track[1], 5, new Scalar(255,255,255),3);
+		    			oldMidpt = nextMidpt;
+			    		//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
+
+	    			}
+	    			*/
+	    			
+	    			
+	    			else{ // if(frame<=544)
+	    				
+	    				track = Functions.trackOrientation(track, exVals);
+	    				Point nextMidpt = Functions.getMidpoint(track);
+	    				//System.out.println(Functions.yDist(nextMidpt));
+	    				System.out.println(Functions.getAngleTwoLines(track)); //prints just angle
+	    				//"frame: "+frame+ " " + 
+	    				oldAngle = newAngle;
+	    				Core.line(video_image, track[0], track[1], new Scalar(0,0,0));
+		    			//Core.circle(video_image, track[1], 5, new Scalar(255,255,255),3);
+		    			oldMidpt = nextMidpt;
+			    		//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
+
+	    			}
+	    			
+	    			
 	    			Core.putText(video_image, "Frame"+frame,new Point(55, 150) , 3,1.0,new Scalar(255,255,255),3); //for test
 
-	    			//Point tster = new Point(600,650);
-	    			//Core.circle(video_image, tster, 30, new Scalar(255,255,255));
-    	     		//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
-
-    	     		//maxArea=Imgproc.contourArea(largest_contour);
-		    		System.out.println(maxArea);
+	    		if(frame>=1){//frame>=250 || frame <= 300){ //void frame 415
+		    		Functions.displayImage(Functions.Mat2BufferedImage(video_image));
 		    		
-		    		  //allArea.append(maxArea).append("\n");
-	    			//bw.newLine();
-	    			
-	    			//bw.write(String.valueOf(maxArea));
-	    			
-	    			//System.out.println("frame "+frame);
-    	     		
-    	     		
-	    			
-		      }
-		     } 
-	       
+		    		try {
+						Thread.sleep(1);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+		    	}
+	    		
+}
+    		
+		    		
+	    		}
+	    		
+		      
+   	     		//Rect maxRect = Imgproc.boundingRect(largest_contour); //for testing
+   	     		//Core.rectangle(video_image, new Point(maxRect.x,maxRect.y), new Point(maxRect.x+maxRect.width,maxRect.y+maxRect.height), new Scalar(255,0,0), 3); //for testing
+	    		//Point tster = new Point(700, 100);
+	    		//Core.circle(video_image, tster, 5, new Scalar(255,255,255));
+	    		//Point tster = new Point(600,650);
+	    		//Core.circle(video_image, tster, 30, new Scalar(255,255,255));
+   	     		//Functions.displayImage(Functions.Mat2BufferedImage(video_image));
 
-		//}catch (InterruptedException e) {}
-	    		   	  /** 
-	    		   	 catch (IOException ioe) {
-	    		   	ioe.printStackTrace();
-			}
-	    		   	finally
-	    		   	{ 
-	    		   	   try{
-	    		   	      if(bw!=null)
-	    		   		 bw.close();
-	    		   	   }catch(Exception ex){
-	    		   	       System.out.println("Error in closing the BufferedWriter"+ex);
-	    		   	    }
-	    		   	}
-	    		   	
-	    		   	
-	    		   	*/
-	    		   	
-	    		      }}
-	    		   
-	    			
-	    			
-	    			
-//271 frame weird contour
+	    		 //end of if contours2 size checking
+		      }
+		      
+		     } 
+
+
+}
+	      
